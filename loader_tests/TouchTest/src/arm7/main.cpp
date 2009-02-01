f99ac21ea172ba1852f0c5b8a2573e3881a41e6f
Erik Faye-Lund <kusmabite@gmail.com>
2009-02-01 19:08:20 +0100

    updating input handling to devkitARM r24 libnds

--- loader_tests/TouchTest/src/arm7/main.cpp
+++ loader_tests/TouchTest/src/arm7/main.cpp
@@ -4,42 +4,7 @@
 touchPosition first,tempPos;
 
 void VcountHandler() {
-//---------------------------------------------------------------------------------
-	static int lastbut = -1;
-	
-	uint16 but=0, x=0, y=0, xpx=0, ypx=0, z1=0, z2=0;
-
-	but = REG_KEYXY;
-
-	if (!( (but ^ lastbut) & (1<<6))) {
- 
-		tempPos = touchReadXY();
-
-		if ( tempPos.x == 0 || tempPos.y == 0 ) {
-			but |= (1 <<6);
-			lastbut = but;
-		} else {
-			x = tempPos.x;
-			y = tempPos.y;
-			xpx = tempPos.px;
-			ypx = tempPos.py;
-			z1 = tempPos.z1;
-			z2 = tempPos.z2;
-		}
-		
-	} else {
-		lastbut = but;
-		but |= (1 <<6);
-	}
-
-	IPC->touchX			= x;
-	IPC->touchY			= y;
-	IPC->touchXpx		= xpx;
-	IPC->touchYpx		= ypx;
-	IPC->touchZ1		= z1;
-	IPC->touchZ2		= z2;
-	IPC->buttons		= but;
-
+	inputGetAndSend();
 }
 
 int main()
